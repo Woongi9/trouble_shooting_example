ad221bc91cfd520a33031c07d29e246dab287a31
Woong <Woongi9@naver.com>
2024-05-11 16:54:46 +0900

    [INIT] - JPA Persistence Context simultaneous save issue

---
+++ jpa_context/src/main/java/com/trouble_shooting/jpa_context/JpaContextApplication.java
@@ -0,0 +1,13 @@
+package com.trouble_shooting.jpa_context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaContextApplication.class, args);
+	}
+
+}
---
+++ jpa_context/src/test/java/com/trouble_shooting/jpa_context/JpaContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trouble_shooting.jpa_context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
